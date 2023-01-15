2195a6f00109ffcb4fcc5a2112c530a402d50ced
yunusfebriansyah <febriansyahyunus369@gmail.com>
2023-01-15 21:55:34 +0700

    14 Fragment

---
+++ 14-fragment/app/src/main/java/com/belajarpemrogramanmobile/LinearLayoutActivity.java
@@ -0,0 +1,14 @@
+package com.belajarpemrogramanmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinearLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+    }
+}
